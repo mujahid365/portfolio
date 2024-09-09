2d9ce2313b2d1ef9a0fdb6c79a05c3e7834d2707
mujahid365 <munnashaikh2003786@gmail.com>
2024-09-09 21:05:32 +0530

    hh

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ db = SQLAlchemy(app)
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fullname = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    phone = db.Column(db.String(11), unique=True, nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(11), nullable=False)
     message = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
